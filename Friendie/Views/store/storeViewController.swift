78056e4de1a75ae87e6e805cc4d7df3f7d5f8940
BB <496127893@qq.com>
2020-05-30 14:51:17 +0800

    update

---
+++ Friendie/Views/store/storeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  storeViewController.swift
+//  Friendie
+//
+//  Created by BB on 2020/5/30.
+//  Copyright © 2020 Com. All rights reserved.
+//
+
+import UIKit
+
+class storeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backhome(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
